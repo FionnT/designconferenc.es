bb96547e8ec4cfed8925aa981a8f2907d0f3d943
Fionn Tolster <fionntolster@invisionapp.com>
2019-08-12 18:04:43 +0200

    stfa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.set('views', './pages')
 app.set('view engine', 'pug')
 
 
-app.listen(port, () => console.log(`Running on port ${port}!`))
+// app.listen(port, () => console.log(`Running on port ${port}!`))
 
 app.use(require('cookie-parser')());
 app.use(require('express-session')({
@@ -34,3 +34,41 @@ app.use('/', require('./modules/commands/purge'));
 app.get('*', function(req, res) {
   res.redirect('/404');
 });
+
+
+// var app = require('../designconferenc.es');
+// var http = require('http');
+// var cluster = require('cluster');
+//
+// var port = process.env.PORT || '80';
+// app.set('port', port);
+//
+//
+// // Code to run if we're in the master process
+// if (cluster.isMaster) {
+//
+//   var cpuCount = require('os').cpus().length;
+//
+//   // Create a worker for each CPU
+//   for (var i = 0; i < cpuCount; i += 1) {
+//       cluster.fork();
+//   }
+//
+// } else {
+//   var server = http.createServer(app);
+//   server.listen(port);
+//   server.on('listening', onListening);
+// }
+//
+// cluster.on('exit', function(worker) {
+//   console.log('Worker ' + worker.id + ' died :(');
+//   cluster.fork();
+// });
+//
+// function onListening() {
+//   var addr = server.address();
+//   var bind = typeof addr === 'string'
+//     ? 'pipe ' + addr
+//     : 'port ' + addr.port;
+//     console.log('Listening on ' + bind);
+// }
