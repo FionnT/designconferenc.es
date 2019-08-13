9b94d1c314b59acd072c49d988d9867b91b08de3
Fionn Tolster <fionntolster@invisionapp.com>
2019-08-13 13:41:37 +0200

    Port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express');
 const app = express();
-var port = process.env.PORT || 8080;
+const port = 80;
 const passport = require('passport');
 
 
@@ -34,41 +34,3 @@ app.use('/', require('./modules/commands/purge'));
 app.get('*', function(req, res) {
   res.redirect('/404');
 });
-
-
-// var app = require('../designconferenc.es');
-// var http = require('http');
-// var cluster = require('cluster');
-//
-// var port = process.env.PORT || '80';
-// app.set('port', port);
-//
-//
-// // Code to run if we're in the master process
-// if (cluster.isMaster) {
-//
-//   var cpuCount = require('os').cpus().length;
-//
-//   // Create a worker for each CPU
-//   for (var i = 0; i < cpuCount; i += 1) {
-//       cluster.fork();
-//   }
-//
-// } else {
-//   var server = http.createServer(app);
-//   server.listen(port);
-//   server.on('listening', onListening);
-// }
-//
-// cluster.on('exit', function(worker) {
-//   console.log('Worker ' + worker.id + ' died :(');
-//   cluster.fork();
-// });
-//
-// function onListening() {
-//   var addr = server.address();
-//   var bind = typeof addr === 'string'
-//     ? 'pipe ' + addr
-//     : 'port ' + addr.port;
-//     console.log('Listening on ' + bind);
-// }
